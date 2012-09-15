f471cc7ab216d3b476a124f1dcddf51aa6b22f95
Ivan Madrid <ivanmadrid@MacBook-Lars.local>
2012-09-14 21:23:58 -0500

    agregado el SearchViewController (PopUp para SearchBar con IBaction)

---
+++ EPubReader/SearchTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTextViewController.h
+//  Editorial Digital
+//
+//  Created by Ivan Madrid on 14/09/12.
+//  Copyright (c) 2012 David Díaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTextViewController : UIViewController
+
+@end
---
+++ EPubReader/SearchTextViewController.m
@@ -0,0 +1,44 @@
+//
+//  SearchTextViewController.m
+//  Editorial Digital
+//
+//  Created by Ivan Madrid on 14/09/12.
+//  Copyright (c) 2012 David Díaz. All rights reserved.
+//
+
+#import "SearchTextViewController.h"
+
+@interface SearchTextViewController ()
+
+@end
+
+@implementation SearchTextViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
